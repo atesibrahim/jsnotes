4f26fadb261c241765917790c25f56a801226643
atesibrahim <ates.ibrahimhalil@gmail.com>
2020-11-25 13:37:06 +0000

    added new test

---
+++ test/unit-test.js
@@ -0,0 +1,10 @@
+// Find the smallest avaliable piece
+// test just that
+// mock everything else
+
+// functional testing
+// all together now
+// start on outside, end on the outside
+// block box testing
+// make sure everyting fits together
+
